d0274394bbe630db944dfd82f1147d11191cb8dc
Catchco1 <cag107@gmail.com>
2019-10-23 13:15:23 -0400

    Add gitignore

--- musicSuggester/server.js
+++ musicSuggester/server.js
@@ -6,4 +6,7 @@ const port = 3000
 
 app.get('/', (req,res)  => res.send('Welcome to Music Suggester'));
 
-app.listen(port, () => console.log('Music suggester listening on port ${port}'));
+app.get('/route', function(req, res){
+    res.send('This is routing using Node and Express')
+});
+app.listen(port, () => console.log(`Music suggester listening on port ${port}`));
