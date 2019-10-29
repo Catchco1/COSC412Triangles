133d4252ad62ac79ed32d439190c17d0d3fed571
Catchco1 <cag107@gmail.com>
2019-10-29 18:44:26 -0400

    Add remote database

--- musicSuggester/config/db.js
+++ musicSuggester/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url : 'mongodb://localhost:27017/bands'
+    //url : 'mongodb://localhost:27017/bands'
+    url : 'mongodb+srv://Root:Din0saur@cluster0-lakt4.mongodb.net/test?retryWrites=true&w=majority'
  }
--- musicSuggester/models/bands.js
+++ musicSuggester/models/bands.js
@@ -2,6 +2,6 @@ var mongoose = require('mongoose');
 
 // define our bands model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('bandsCollection', {
+module.exports = mongoose.model('songs', {
    _id : {type : String, default: ''}
-}, "bandsCollection");
+}, "songobj");
