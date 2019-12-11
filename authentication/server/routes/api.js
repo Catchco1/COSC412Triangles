d6359919122f65ce932395f71120b1df6aee3592
Katya Doersam <katyadoersam@Katyas-MacBook-Pro.local>
2019-12-11 18:19:34 -0500

    Fix login

--- authentication/server/routes/api.js
+++ authentication/server/routes/api.js
@@ -4,16 +4,14 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 const jwt = require('jsonwebtoken')
 const db = "mongodb+srv://Root:Din0saur@cluster0-lakt4.mongodb.net/musicApp?retryWrites=true&w=majority";
-
-
 // mongoose.Promise = global.Promise;
 
 mongoose.connect(db, function(err){
-    if(err){
-        console.error('Error! ' + err)
-    } else {
-      console.log('Connected to mongodb')
-    }
+  if(err){
+    console.error('Error! ' + err)
+  } else {
+    console.log('Connected to mongodb')
+  }
 });
 
 function verifyToken(req, res, next) {
@@ -33,13 +31,87 @@ function verifyToken(req, res, next) {
 }
 
 router.get('/events', (req,res) => {
-
+  let events = [
+    {
+      "_id": "1",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "2",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "3",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "4",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "5",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "6",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    }
+  ]
   res.json(events)
 })
 
-router.get('/account', verifyToken, (req, res) => {
-
-  res.json(account)
+router.get('/special', verifyToken, (req, res) => {
+  let specialEvents = [
+    {
+      "_id": "1",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "2",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "3",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "4",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "5",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "6",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    }
+  ]
+  res.json(specialEvents)
 })
 
 router.post('/register', (req, res) => {
@@ -57,27 +129,23 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-  console.log("posted to database")
   let userData = req.body
   User.findOne({email: userData.email}, (err, user) => {
-    if (user===null) {
-      return res.status(400).send({
-        message : "User not found."
-      });
+    if (err) {
+      console.log(err)
     } else {
-      if (user.ValidPassword(userData.password)) {
-        return res.status(201).send({
-          message : "User logged in",
-        })
+      if (!user) {
+        res.status(401).send('Invalid Email')
+      } else
+      if ( user.password !== userData.password) {
+        res.status(401).send('Invalid Password')
       } else {
-        return res.status(400).send({
-          message : "wrong password"
-        });
+        let payload = {subject: user._id}
+        let token = jwt.sign(payload, 'secretKey')
+        res.status(200).send({token})
       }
     }
-  });
+  })
 })
-
-
 
 module.exports = router;
